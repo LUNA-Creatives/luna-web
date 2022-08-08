b561bbc0ac4857a0af57a8255e4f827adc4f68cd
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-08 08:09:22 +0200

    feat: icons

---
+++ src/utils/images.ts
@@ -0,0 +1,7 @@
+export const LUNA_ICON_BLACK = '/luna-icon-black.png';
+export const LUNA_ICON_BLACK_SVG = '/luna-icon-black.svg';
+export const LUNA_ICON_BLACK_COLOR = '/luna-icon-black-color.png';
+export const LUNA_ICON_BLACK_COLOR_SVG = '/luna-icon-black-color.svg';
+
+export const LUNA_ICON_INVERTED_COLOR = '/luna-icon-inverted-color.png';
+export const LUNA_ICON_INVERTED_COLOR_SVG = '/luna-icon-inverted-color.svg';
