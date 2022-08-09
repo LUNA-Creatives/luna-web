2a9ae595cd293a5a4b37aa1fbee2a20d842bdc7c
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-09 16:50:35 +0200

    refactor: update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import { HeaderSection } from './components/HeaderSection';
+import { InfoSection } from './components/InfoSection';
 import { ThemeConfig } from './theme';
 
 const App = () => {
+  const [visible, isVisible] = useState(true);
+  useEffect(() => {
+    setTimeout(function () {
+      isVisible(false);
+    }, 7000);
+  }, []);
   return (
     <ThemeConfig>
       <HeaderSection />
+      {!visible && <InfoSection />}
     </ThemeConfig>
   );
 };
