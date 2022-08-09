c1b5b82c66334ab0e8a729b19c5108cbe2329644
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-09 16:50:15 +0200

    feat: infosection

---
+++ src/components/InfoSection/InfoSection.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { Box, Container, Typography } from '@material-ui/core';
+import { motion } from 'framer-motion';
+import lottie from 'lottie-web/build/player/lottie_svg';
+
+import useStyles from './style';
+import flowyAnimation from '../../utils/animations/flowyAnimation.json';
+
+export const InfoSection = () => {
+  const classes = useStyles();
+
+  const flowyAnimationId = 'flowyAnimation';
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector(`#${flowyAnimationId}`) as Element,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: flowyAnimation,
+    });
+  }, []);
+
+  return (
+    <Container className={classes.container}>
+      <Typography
+        variant="h4"
+        className={classes.headline}
+        component={motion.div}
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        Framtidens techpartner. Utvecklar appar och hemsidor.
+      </Typography>
+      <Box className={classes.animation} id={flowyAnimationId} />
+    </Container>
+  );
+};
