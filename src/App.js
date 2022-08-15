bbc2afb96c9cc5e8334885f854e58a7ea9cf9da7
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-15 07:13:43 +0200

    feat: homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react';
-import { HeaderSection } from './components/HeaderSection';
-import { InfoSection } from './components/InfoSection';
 import { ThemeConfig } from './theme';
+import { HomePage } from './pages/HomePage';
 
 const App = () => {
-  const [visible, isVisible] = useState(true);
-  useEffect(() => {
-    setTimeout(function () {
-      isVisible(false);
-    }, 7000);
-  }, []);
   return (
     <ThemeConfig>
-      <HeaderSection />
-      {!visible && <InfoSection />}
+      <HomePage />
     </ThemeConfig>
   );
 };
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { HeaderSection } from '../../components/HeaderSection';
+import { InfoSection } from '../../components/InfoSection';
+import { TeamSection } from '../../components/TeamSection';
+
+export const HomePage = () => {
+  const [visible, isVisible] = useState(true);
+  const [team, setTeam] = useState([]);
+
+  const fetchAndSetUsers = async () => {
+    try {
+      const { data } = await axios.get('/api/team-member');
+      setTeam(data as any);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(function () {
+      isVisible(false);
+    }, 7000);
+
+    fetchAndSetUsers();
+  }, []);
+
+  return (
+    <>
+      <HeaderSection />
+      {!visible && (
+        <>
+          <InfoSection />
+          <TeamSection data={team} />
+        </>
+      )}
+    </>
+  );
+};
