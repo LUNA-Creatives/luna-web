bbda01c0b6e794e06cc7a599cdee1425f3418909
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-17 10:33:20 +0200

    feat: constants and logo constants

---
+++ src/utils/data/constants.ts
@@ -0,0 +1,7 @@
+export const constants = {
+  infoSection: {
+    headline: 'Framtidens techpartner. Vi utvecklar appar och hemsidor',
+  },
+  showcaseSection: { headline: 'Våra uppdrag' },
+  teamSection: { headline: 'Vårt team' },
+};
---
+++ src/utils/data/logos.ts
@@ -0,0 +1,14 @@
+import polestar from '../../assets/polestar.webp';
+import gw from '../../assets/gw.webp';
+import timetotell from '../../assets/timetotell.webp';
+
+export const logos = [
+  { id: 1, url: polestar, alt: 'polestar' },
+  { id: 2, url: gw, alt: 'Generation Waste' },
+  { id: 3, url: timetotell, alt: 'Time to tell' },
+  { id: 4, url: gw, alt: 'Generation Waste' },
+  { id: 5, url: timetotell, alt: 'Time to tell' },
+  { id: 6, url: polestar, alt: 'Polestar' },
+  { id: 7, url: gw, alt: 'Generation Waste' },
+  { id: 8, url: timetotell, alt: 'Time to tell' },
+];
