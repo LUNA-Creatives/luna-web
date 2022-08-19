708b41d86d9d242280f8a584b0a37e6ea376dc17
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-19 13:40:47 +0200

    refactor: added assets

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Container } from '@material-ui/core';
 import { motion } from 'framer-motion';
 
 import useStyles from './style';
-import { LUNA_ICON_INVERTED_COLOR_SVG } from '../../utils/images';
+import logo from '../../assets/logos/luna-icon-inverted-color.svg';
 
 export const Navbar = () => {
   const classes = useStyles();
@@ -25,7 +25,7 @@ export const Navbar = () => {
           repeatType: 'reverse',
         }}
       >
-        <img alt="logo" height={35} src={LUNA_ICON_INVERTED_COLOR_SVG} />
+        <img alt="logo" height={35} src={logo} />
       </motion.div>
     </Container>
   );
