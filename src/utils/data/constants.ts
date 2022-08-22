2e2d0bfe167866b9827ee0efed26d3223bfeaa93
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-22 13:42:14 +0200

    refactor: delete unused data

--- src/utils/data/constants.ts
+++
@@ -1,7 +0,0 @@
-export const constants = {
-  infoSection: {
-    headline: 'Framtidens techpartner. Vi utvecklar appar och hemsidor',
-  },
-  showcaseSection: { headline: 'Våra uppdrag' },
-  teamSection: { headline: 'Vårt team' },
-};
