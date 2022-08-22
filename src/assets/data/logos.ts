f9735c9c828982386dcb6cd4b2b7ef621ba77e42
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-22 13:07:28 +0200

    refactor: clientLogos

--- src/assets/data/logos.ts
+++ src/assets/data/logos.ts
@@ -1,7 +1,7 @@
-import polestar from '../../assets/polestar.webp';
-import gw from '../../assets/GW.svg';
-import timetotell from '../../assets/timetotell.png';
-import SempreInter from '../../assets/SempreInter.svg';
+import polestar from '../../assets/ClientLogos/polestar.webp';
+import gw from '../../assets/ClientLogos/GW.svg';
+import timetotell from '../../assets/ClientLogos/timetotell.png';
+import SempreInter from '../../assets/ClientLogos/SempreInter.svg';
 
 export const logos = [
   { id: 1, url: polestar, alt: 'Polestar' },
