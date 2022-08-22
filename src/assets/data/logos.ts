50e07411b3c5d6138d3f1ba97f48bfe00d2cb0cb
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-08-22 13:02:08 +0200

    feat: clientlogos

---
+++ src/assets/data/logos.ts
@@ -0,0 +1,13 @@
+import polestar from '../../assets/polestar.webp';
+import gw from '../../assets/GW.svg';
+import timetotell from '../../assets/timetotell.png';
+import SempreInter from '../../assets/SempreInter.svg';
+
+export const logos = [
+  { id: 1, url: polestar, alt: 'Polestar' },
+  { id: 2, url: gw, alt: 'Generation Waste' },
+  { id: 3, url: timetotell, alt: 'Time to tell' },
+  { id: 4, url: SempreInter, alt: 'SempreInter' },
+  { id: 5, alt: 'We Are Women' },
+  { id: 6, alt: 'Bravais Arc' },
+];
