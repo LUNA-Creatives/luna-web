651dd59e0b52df597350ffcc72c88ab56d67df18
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-09-01 14:00:12 +0200

    refactor: clean out comments

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const { api, data, http } = require('@serverless/cloud');
 http.on(404, 'index.html');
 
-// Create GET route and return users
 api.get('/api/team-member', async (req, res) => {
-  // Get users from Serverless Data
   let result = await data.get('teamMember:*', true);
-  // Return the results
   res.send(result.items);
 });
 
-// Create GET route and return users
 api.get('/api/icons', async (req, res) => {
-  // Get users from Serverless Data
   let result = await data.get('icon:*', true);
-  // Return the results
   res.send(result.items);
 });
 
-// Catch all for missing API routes
 api.get('/api/*', (req, res) => {
   console.log(`404 - api`);
   res.status(404).send({ error: 'not found' });
