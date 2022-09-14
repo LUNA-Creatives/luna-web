59b47eaf1baecdb018abe957ff6a825f8f8dd15e
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-09-14 09:39:07 +0200

    refactor: 3d text data

--- src/components/Sections/ThreeCanvas/TextGroup.jsx
+++ src/components/Sections/ThreeCanvas/TextGroup.jsx
@@ -1,13 +1,14 @@
 import {  useRef } from 'react';
 import { Text } from './Text';
+import data from '../../../assets/data/data.json'
 
 export const TextGroup = () => {
     const ref = useRef();
  
     return (
       <group ref={ref} scale={0.1}>
-        <Text hAlign="right"  position={[-20, 0, 53]} children="FRAMTIDENS" />
-        <Text hAlign="right"  position={[-30, -6.5, 53]} children="TECHPARTNER" />
+        <Text hAlign="right"  position={[-20, 0, 53]} children={data.ThreeCanvas.headlineOne} />
+        <Text hAlign="right"  position={[-30, -6.5, 53]} children={data.ThreeCanvas.headlineTwo} />
       </group>
     );
   }
