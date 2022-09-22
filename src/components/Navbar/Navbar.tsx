b741240daa0bc1ff0846c4955b0ad1a9140f1733
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-09-22 10:22:13 +0200

    feat: navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,47 +1,79 @@
-import { useEffect } from 'react';
-import { Container, Box } from '@mui/material';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { Grid, Container, Box } from '@mui/material';
 import lottie from 'lottie-web/build/player/lottie_svg';
+import clsx from 'clsx';
 
 import useStyles from './style';
 import { INavbar } from './types';
 import animation from '../../assets/animations/logoLoopingGlitchAnimation.json';
+import { Overlay } from './Overlay';
 
-export const Navbar = ({ logo }: INavbar) => {
+export const Navbar = ({ data }: INavbar) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [showLogo, setShowLogo] = useState(true);
+
   const classes = useStyles();
-
   const animationId = 'animation';
 
   useEffect(() => {
-    lottie.loadAnimation({
+    const instance = lottie.loadAnimation({
       container: document.querySelector(`#${animationId}`) as Element,
       renderer: 'svg',
       loop: true,
       autoplay: true,
       animationData: animation,
     });
+
+    const handleScroll = () => {
+      const hide = window.scrollY > 50;
+      if (hide) {
+        setShowLogo(false);
+      } else {
+        setShowLogo(true);
+      }
+    };
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      instance.destroy();
+      document.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
-    <Container
-      component={motion.div}
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.6 }}
-      className={classes.container}
-    >
-      <motion.div
-        initial={{ y: -5 }}
-        animate={{ y: 0 }}
-        transition={{
-          type: 'Inertia',
-          repeat: Infinity,
-          duration: 1,
-          repeatType: 'reverse',
-        }}
-      >
-        <Box component={'div'} className={classes.animation} id={animationId} />
-      </motion.div>
-    </Container>
+    <>
+      <Grid className={classes.root}>
+        {showOverlay && <Overlay data={data} />}
+        <Container className={classes.container}>
+          <Box
+            component={'div'}
+            className={clsx(classes.animation, {
+              [classes.disabled]: showOverlay || !showLogo,
+            })}
+            id={animationId}
+          />
+          <Box
+            onClick={() => setShowOverlay(true)}
+            component={'div'}
+            className={clsx(classes.menuIcon, {
+              [classes.disabled]: showOverlay,
+            })}
+          >
+            <span className={`${classes.line} ${classes.left}`} />
+            <span className={classes.line} />
+            <span className={`${classes.line} ${classes.right}`} />
+          </Box>
+          <Box
+            onClick={() => setShowOverlay(false)}
+            component={'div'}
+            className={clsx(classes.menuIcon, classes.closeIcon, {
+              [classes.disabled]: !showOverlay,
+            })}
+          >
+            <span className={`${classes.line} ${classes.rotateRight}`} />
+            <span className={`${classes.line} ${classes.rotateLeft}`} />
+          </Box>
+        </Container>
+      </Grid>
+    </>
   );
 };
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,23 +1,86 @@
 import { createStyles, makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 
+import { ipadBreakpoint } from '../../utils/screenSizeBreakpoints';
+
 export default makeStyles((theme: Theme) =>
   createStyles({
-    container: {
-      position: 'fixed',
-      right: 0,
-      top: 0,
-      margin: 15,
-
+    root: {
+      minWidth: '100vw',
       display: 'flex',
       justifyContent: 'center',
-
-      width: 0,
-      zIndex: 9999,
+    },
+    container: {
+      position: 'fixed',
+      top: 0,
+      display: 'flex',
+      maxWidth: 1600,
+      zIndex: 9000,
+    },
+    overlay: {
+      display: 'flex',
+      alignItems: 'center',
+      zIndex: 8500,
+      position: 'fixed',
+      minWidth: '100vw',
+      top: 0,
+      height: '100vh',
+      backgroundColor: theme.palette.primary.main,
+      opacity: 1,
+      transition: 'opacity 1s ease',
     },
     animation: {
-      padding: 0,
       width: 150,
+      height: 80,
+      marginLeft: '-60px',
+      zIndex: 9000,
+      margin: 5,
+      [theme.breakpoints.up(ipadBreakpoint)]: {
+        marginLeft: '-70px',
+      },
+    },
+    menuIcon: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      zIndex: 9999,
+      height: 30,
+      width: 30,
+      position: 'absolute',
+      right: 15,
+      top: 15,
+      cursor: 'pointer',
+      margin: 10,
+    },
+    line: {
+      height: 2,
+      width: 30,
+      display: 'block',
+      marginBottom: 4,
+      backgroundColor: theme.palette.primary.main,
+    },
+    left: {
+      width: 15,
+      backgroundColor: theme.palette.primary.main,
+    },
+    right: {
+      width: 15,
+      backgroundColor: theme.palette.primary.main,
+    },
+    closeIcon: {
+      zIndex: 9999,
+    },
+    rotateRight: {
+      backgroundColor: theme.palette.primary.dark,
+      transform: 'translate(0px, 6px) rotate(-45deg)',
+    },
+    rotateLeft: {
+      backgroundColor: theme.palette.primary.dark,
+      transform: 'translate(0px, 0px) rotate(45deg)',
+    },
+    disabled: {
+      display: 'none',
+      transition: '.2s ease',
     },
   })
 );
