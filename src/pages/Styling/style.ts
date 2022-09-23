04a6f27492659f965a8197d76aaf8da78d226092
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-09-23 07:57:37 +0200

    feat: page styling

---
+++ src/pages/Styling/style.ts
@@ -0,0 +1,11 @@
+import { createStyles, makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+
+export default makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.primary.dark,
+      paddingTop: '5rem',
+    },
+  })
+);
