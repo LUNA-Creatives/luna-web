bfeb6f3d9fda76b10b00eea7e3a0e509c59e32aa
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-10-06 14:18:52 +0200

    refactor: remove parallax

--- src/components/Sections/Service/Information/Information.tsx
+++ src/components/Sections/Service/Information/Information.tsx
@@ -1,5 +1,4 @@
 import { Grid, Container, Box, Typography } from '@mui/material';
-import { Parallax } from 'react-scroll-parallax';
 
 import { IInformation, Column, Item } from './types';
 import useStyles from './style';
@@ -8,57 +7,51 @@ export const Information = ({ data, headline }: IInformation) => {
   const classes = useStyles();
 
   return (
-    <Parallax translateY={[0, -100]} className={classes.parallax}>
-      <Grid className={classes.root}>
-        <Container className={classes.container}>
-          <Box component={'div'}>
-            <Typography
-              key={headline}
-              className={classes.headline}
-              variant={'h1'}
-            >
-              {headline}
-            </Typography>
-          </Box>
+    <Grid className={classes.root}>
+      <Container className={classes.container}>
+        <Box component={'div'}>
+          <Typography
+            key={headline}
+            className={classes.headline}
+            variant={'h1'}
+          >
+            {headline}
+          </Typography>
+        </Box>
 
-          <Box component={'div'} className={classes.flexBox}>
-            {data.map((column: Column) => (
-              <Box
-                key={column.id}
-                component={'div'}
-                className={classes.textBox}
-              >
-                <Typography className={classes.upperCase} variant={'subtitle1'}>
-                  {column.subHeadline}
+        <Box component={'div'} className={classes.flexBox}>
+          {data.map((column: Column) => (
+            <Box key={column.id} component={'div'} className={classes.textBox}>
+              <Typography className={classes.upperCase} variant={'subtitle1'}>
+                {column.subHeadline}
+              </Typography>
+              {column.text1 && (
+                <Typography className={classes.text} variant={'body1'}>
+                  {column.text1}
                 </Typography>
-                {column.text1 && (
-                  <Typography className={classes.text} variant={'body1'}>
-                    {column.text1}
-                  </Typography>
-                )}
-                {column.text2 && (
-                  <Typography
-                    className={`${classes.text}, ${classes.bold}`}
-                    variant={'body1'}
-                  >
-                    {column.text2}
-                  </Typography>
-                )}
-                {column.listItems?.map((item: Item) => (
-                  <Box
-                    key={item.id}
-                    component={'div'}
-                    className={classes.inlineBox}
-                  >
-                    <li></li>
-                    <Typography variant={'body1'}>{item.text}</Typography>
-                  </Box>
-                ))}
-              </Box>
-            ))}
-          </Box>
-        </Container>
-      </Grid>
-    </Parallax>
+              )}
+              {column.text2 && (
+                <Typography
+                  className={`${classes.text}, ${classes.bold}`}
+                  variant={'body1'}
+                >
+                  {column.text2}
+                </Typography>
+              )}
+              {column.listItems?.map((item: Item) => (
+                <Box
+                  key={item.id}
+                  component={'div'}
+                  className={classes.inlineBox}
+                >
+                  <li></li>
+                  <Typography variant={'body1'}>{item.text}</Typography>
+                </Box>
+              ))}
+            </Box>
+          ))}
+        </Box>
+      </Container>
+    </Grid>
   );
 };
