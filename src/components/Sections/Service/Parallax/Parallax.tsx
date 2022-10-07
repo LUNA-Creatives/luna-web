117627892726b1035b6faacc3ab6567c49f38223
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-10-07 14:04:35 +0200

    refactor: parallax

--- src/components/Sections/Service/Parallax/Parallax.tsx
+++ src/components/Sections/Service/Parallax/Parallax.tsx
@@ -1,18 +1,27 @@
-import { Grid, Container } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Grid, useMediaQuery } from '@mui/material';
 import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
 
 import { IHeader } from './types';
 import useStyles from './style';
+import { ipadBreakpoint } from '../../../../utils/screenSizeBreakpoints';
 
 export const Parallax = ({ data }: IHeader) => {
   const classes = useStyles();
+  const isDesktop = useMediaQuery(`(min-width:${ipadBreakpoint}px)`);
+  const [speedProp, setSpeedProp] = useState(0);
+
+  useEffect(() => {
+    if (isDesktop) {
+      setSpeedProp(-20);
+    }
+  }, [isDesktop]);
 
   return (
     <Grid className={classes.root}>
       <ParallaxBanner className={classes.parallax}>
-        <ParallaxBannerLayer image={data.imageSource} speed={-20} />
+        <ParallaxBannerLayer image={data.imageSource} speed={speedProp} />
       </ParallaxBanner>
-      <Container className={classes.container}></Container>
     </Grid>
   );
 };
