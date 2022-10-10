127187e61c5411f14e7b5e305581be3059259158
Fanny Mårtensson <fanny.martensson@gmail.com>
2022-10-10 20:03:43 +0200

    fix: portfolio types

--- src/pages/ServicePage/types.ts
+++ src/pages/ServicePage/types.ts
@@ -1,6 +1,7 @@
 import { Column } from '../../components/Sections/Service/Information/types';
 import { Card } from '../../components/Sections/Service/OurOffers/Card/types';
 import { Header } from '../../components/Sections/Service/Parallax';
+import { Portfolio } from '../../components/Sections/Service/Portfolio/types';
 
 export interface IServicePage {
   slug: string;
@@ -8,4 +9,5 @@ export interface IServicePage {
   header: Header;
   information: Column[];
   offers?: Card[];
+  portfolio: Portfolio;
 }
